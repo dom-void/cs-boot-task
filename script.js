49c6e1db5a8b02b6e2e712a5cb1c9597927d0ff9
dom void <doe@onet.pl>
2020-12-21 09:37:08 +0100

    small fix

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const getParams = window.location.search;
 const urlParams = new URLSearchParams(getParams);
 
-console.dir(urlParams);
-
 let reqID = null;
 let userID = null;
 
@@ -14,7 +12,7 @@ if (urlParams.has('user')) {
     userID = urlParams.get('user');
 }
 
-console.log('id:', reqID, 'user:', userID);
+console.log('id:', reqID+';', 'user:', userID);
 
 //TODO: get data for reqID from json, disable all fields (change styling), fill all the fields with data
 //TODO: if userID is owner, anable all the fields (change styling)
