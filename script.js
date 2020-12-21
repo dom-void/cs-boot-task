1adb1844210007004f11eb05d6ea4ee9ef45db23
dom void <doe@onet.pl>
2020-12-21 09:21:45 +0100

    implemented getting url parameters

---
+++ script.js
@@ -0,0 +1,23 @@
+const getParams = window.location.search;
+const urlParams = new URLSearchParams(getParams);
+
+console.dir(urlParams);
+
+let reqID = null;
+let userID = null;
+
+if (urlParams.has('id')) {
+    reqID = urlParams.get('id');
+}
+
+if (urlParams.has('user')) {
+    userID = urlParams.get('user');
+}
+
+console.log('id:', reqID, 'user:', userID);
+
+//TODO: get data for reqID from json, disable all fields (change styling), fill all the fields with data
+//TODO: if userID is owner, anable all the fields (change styling)
+
+const apiURL = "http://localhost:3000/"
+
