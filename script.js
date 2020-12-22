2c8eebc89a2fe48bc80c21e899797fa5a9a82f68
dom void <doe@onet.pl>
2020-12-22 18:47:31 +0100

    dates setting and deadline ranges added

--- script.js
+++ script.js
@@ -20,10 +20,14 @@ let fourWeeksAhead = new Date(currentDate + 28 * 24 * 60 * 60 * 1000);
 let fourWeeksAheadY = fourWeeksAhead.getFullYear();
 let fourWeeksAheadM = fourWeeksAhead.getMonth()+1;
 let fourWeeksAheadD = fourWeeksAhead.getDate();
+let minDeadline = nextWeekY+'-'+nextWeekM+'-'+nextWeekD;
+let defDeadline = fourWeeksAheadY+'-'+fourWeeksAheadM+'-'+fourWeeksAheadD;
+let maxDeadline = todayY+'-12-31';
 
+//FIXME: delete logs
 console.log('today is:', todayY+'-'+todayM+'-'+todayD);
-console.log('next week is:', nextWeekY+'-'+nextWeekM+'-'+nextWeekD);
-console.log('four weeks ahead is:', fourWeeksAheadY+'-'+fourWeeksAheadM+'-'+fourWeeksAheadD);
+console.log('next week is:', minDeadline);
+console.log('four weeks ahead is:', defDeadline);
 
 if (urlParams.has('id')) {
     reqID = urlParams.get('id');
@@ -33,23 +37,42 @@ if (urlParams.has('user')) {
     userID = urlParams.get('user');
 }
 
+//FIXME: delete log
 console.log('id:', reqID+';', 'user:', userID);
-
-console.log(document.getElementById('reqname'))
 
 //TODO: get data for reqID from json, disable all fields (change styling), fill all the fields with data
 //TODO: if userID is owner, anable all the fields (change styling)
 
 // const apiURL = "http://localhost:3000/"
 
-function weeksAhead(n) {
+displayFormContent();
 
+function displayFormContent() {
+    if (reqID) {
+        if (userID != activeUser) {
+            //TODO: disabling form fields
+            console.log('disabling form fields');
+        }
+        //TODO: writing values to the form fields
+        console.log('writing values to the form fields');
+    } else {
+        if (nextWeekY != todayY) {
+            //TODO: disabling form fields
+            //TODO: display info: 'Sorry, it is too late to finish the project this year. Please come back next year. Happy New Year!'
+        } else {
+            if (fourWeeksAheadY > todayY) {
+                defDeadline = maxDeadline;
+            }
+            renderDeadline(minDeadline, defDeadline, maxDeadline);
+        }
+    }
 }
 
-function renderPageContent () {
-
-    console.log('renderPageContent() works')
-
+function renderDeadline(min, def, max) {
+    let field = document.getElementById('deadline')
+    field.setAttribute('min', min);
+    field.setAttribute('value', def);
+    field.setAttribute('max', max);
 }
 
 function validate(form) {
